7add96203d9a7d43d067b25fd6d878f85e75c606
John Smith <example@stanford.edu>
2013-07-12 04:09:33 +0000

    debugging

--- web.js
+++ web.js
@@ -4,7 +4,13 @@ var app = express.createServer(express.logger());
 
 var out = "message from a variable";
 
+var buf = new buffer(256);
+
+var fs = require('fs');
+
 app.get('/', function(request, response) {
+  buf = fs.readFileSync('index.html');
+  out = buf.toString();
   response.send(out);
 });
 
