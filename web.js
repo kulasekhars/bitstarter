cc77c4fffea1299df3b3722a30cf19f1aad01257
John Smith <example@stanford.edu>
2013-07-12 03:48:59 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buf = new buffer(256)
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    buf = fs.readFileSync('index.html');
+    response.send(buf.toString());     
 });
 
 var port = process.env.PORT || 5000;
